c6ae8566ddd5672bf5984c4cc4396c05cf890fe4
IvanAngoldt <112856964+IvanAngoldt@users.noreply.github.com>
2022-10-19 18:45:28 +0300

    Add files via upload

---
+++ main/task6.js
@@ -0,0 +1,89 @@
+function getPrices() {
+    return {
+        prodTypes: [40000, 45000, 100000],
+        optionPrices: {
+            option2: 2000,
+            option3: 5000
+        },
+        secureOptions: {
+            check1: 5000,
+            check2: 15000
+        }
+    };
+}
+  
+function totalSum(v) {
+    let s = document.getElementsByName("priceRate");
+    let select = s[0];
+    let radioBtn = document.getElementById("optionPrice");
+    radioBtn.style.display = ((select.value === "1" || select.value === "3") ? "none" : "block");
+    let checkBx = document.getElementById("rtx3090");
+    checkBx.style.display = (select.value === "3" ? "block" : "none");
+    let price = 0;
+    let prices = getPrices();
+    let priceIndex = parseInt(select.value) - 1;
+    if (priceIndex >= 0) {
+        price = prices.prodTypes[priceIndex];
+    }
+    if (select.value === "2") {
+        let radio = document.getElementsByName("optionPrice");
+        radio.forEach(function (radio) {
+        if (radio.checked) {
+            let optionPrice = prices.optionPrices[radio.value];
+            if (optionPrice !== undefined) {
+                price += optionPrice;
+            }
+        }
+      });
+    }
+    if (select.value === "3") {
+        let checkbox = document.getElementsByName("rtx3090");
+        checkbox.forEach(function (checkbox) {
+        if (checkbox.checked) {
+            let checkboxPrice = prices.secureOptions[checkbox.value];
+            if (checkboxPrice !== undefined) {
+                price += checkboxPrice;
+            }
+        }
+        });
+    }
+    let forbiddenSymbols = /^(?!(0))\d+$/;
+    let total = document.getElementById("result");
+    if (v.match(forbiddenSymbols) === null) {
+        total.innerHTML = "Число введено некорректно";
+    }
+    else {
+        total.innerHTML = + v * price + " рублей";
+    }
+}
+
+window.addEventListener("DOMContentLoaded", function (event) {
+    let radio = document.getElementById("optionPrice");
+    radio.style.display = "none";
+    let checkbox = document.getElementById("rtx3090");
+    checkbox.style.display = "none";
+    let v = document.getElementById("field");
+    v.addEventListener("input", function (event) {
+        totalSum(v.value);
+    });
+    let s = document.getElementsByName("priceRate");
+    let select = s[0];
+    select.addEventListener("change", function (event) {
+        let target = event.target;
+        totalSum(v.value);
+    });
+    let radioBtn = document.getElementsByName("optionPrice");
+    radioBtn.forEach(function (radio) {
+        radio.addEventListener("change", function (event) {
+            let r = event.target;
+            totalSum(v.value);
+        });
+    });
+    let checkboxBtn = document.getElementsByName("rtx3090");
+    checkboxBtn.forEach(function (checkbox) {
+        checkbox.addEventListener("change", function (event) {
+            let c = event.target;
+            totalSum(v.value);
+        });
+    });
+});
